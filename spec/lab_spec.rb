f36ca10d5ac18f77e3d2dde7cd01acb23210bea4
Bidemi  Ajao <ajaoinsitu@yahoo.co.uk>
2018-10-24 10:46:39 +0000

    automatically backed up by learn

--- spec/lab_spec.rb
+++ spec/lab_spec.rb
@@ -4,5 +4,6 @@ describe 'Your First Learn Lab' do
   it 'contains a file called "my-new-file.txt"' do
     file_added = Dir.glob('my-new-file.txt').size > 0
     expect(file_added).to be_truthy, "Create a new file in this lesson called 'my-new-file.txt' to pass this test"
+    "my-new-file"
   end
 end
